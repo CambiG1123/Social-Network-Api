0f7fff19719f4404b19ffab24b62a5b38508df27
CambiG1123 <campbellgilliland@gmail.com>
2023-11-22 15:19:34 -0500

    created the models

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,9 @@
-require('dotenv').config(
-    {path: '../.env'}
-)
+require('dotenv').config()
 const mongoose = require("mongoose");
 
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
-  {
-    // recommended good practice for compatibility with new versions of mongodb
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
 );
 
 
---
+++ models/Thought.js
@@ -0,0 +1,53 @@
+const {Schema,model} = require('mongoose')
+const moment = require('moment');
+
+// Reaction Schema
+const reactionSchema = new Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => moment(timestamp).format('MMMM Do, YYYY [at] h:mm a'),
+  },
+});
+
+// Thought Schema
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => moment(timestamp).format('MMMM Do, YYYY [at] h:mm a'),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema], // Array of nested documents using the reactionSchema
+});
+
+// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
